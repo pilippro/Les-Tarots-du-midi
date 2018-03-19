252a736acf1a93d5650741510ca5a98d6392a3ad
Philippe PRAUD <p.praud@gmail.com>
2018-03-19 14:20:14 +0100

    first working commit - creating Xcode project

---
+++ Les Tarots du midi/Les Tarots du midi/SecondViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SecondViewController.swift
+//  Les Tarots du midi
+//
+//  Created by Philippe Praud on 19/03/2018.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
