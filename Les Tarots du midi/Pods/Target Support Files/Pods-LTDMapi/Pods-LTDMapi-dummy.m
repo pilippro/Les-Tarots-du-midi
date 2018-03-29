44ad77fa77f4166d61649b74e1b37126fce014c3
LEMORT Maxime <lemort.maxime@icloud.com>
2018-03-29 21:26:58 +0200

    remove Pods/

--- Les Tarots du midi/Pods/Target Support Files/Pods-LTDMapi/Pods-LTDMapi-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_LTDMapi : NSObject
-@end
-@implementation PodsDummy_Pods_LTDMapi
-@end
--- Les Tarots du midi/Pods/Target Support Files/Pods-Les Tarots du midi/Pods-Les Tarots du midi-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Les_Tarots_du_midi : NSObject
-@end
-@implementation PodsDummy_Pods_Les_Tarots_du_midi
-@end
